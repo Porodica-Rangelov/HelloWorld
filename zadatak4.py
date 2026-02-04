38d3cf84dd8ec297f48adb9da323b8b48585a5ad
Rangelov Djole <rangelov.djole@gmail.com>
2026-02-04 17:50:13 +0100

    zadatak4.py added

---
+++ zadatak4.py
@@ -0,0 +1,17 @@
+niz = []
+
+for i in range(10):
+    broj = int(input("Unesi broj: "))
+    niz.append(broj)
+
+print("Pre sortiranja:", niz)
+
+n = len(niz)
+for i in range(n):
+    for j in range(0, n - i - 1):
+        if niz[j] < niz[j + 1]:
+            temp = niz[j]
+            niz[j] = niz[j + 1]
+            niz[j + 1] = temp
+
+print("Posle sortiranja (opadajuće):", niz)
