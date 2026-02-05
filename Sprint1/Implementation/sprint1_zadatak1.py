44bce96eb7156164ebf1ad11570f9b92cdd6ede4
Ivica Rangelov <ivica.rangelov@ingsoftware.com>
2026-02-05 15:52:40 +0100

    sprint1_zadatak1 - work in progress (wip)

---
+++ Sprint1/Implementation/sprint1_zadatak1.py
@@ -0,0 +1,13 @@
+# ### Task 1: Generisanje niza od N random integera [Kompleksnost: 1]
+# - [ ] Uneti broj N od korisnika
+# - [ ] Generisati niz od N random celih brojeva u opsegu od 1 do 100
+# - [ ] Ispisati generisani niz
+
+# **Uputstvo za resavanje:**
+# 1. Koristiti `input()` za unos broja N i konvertovati ga u `int`
+# 2. Importovati `random` modul na pocetku fajla
+# 3. Napraviti praznu listu
+# 4. Koristiti `for` petlju od 0 do N:
+#    - U svakoj iteraciji generisati random broj pomocu `random.randint(1, 100)`
+#    - Dodati broj u listu pomocu `append()`
+# 5. Prikazati generisani niz
