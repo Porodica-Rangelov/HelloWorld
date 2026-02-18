b804ace2eee56e36bb6180eb392926377ea9653a
pilot-ivica-rangelov <44412337+pilot-ivica-rangelov@users.noreply.github.com>
2026-02-18 08:54:35 +0100

    Create upload_to_github.py

---
+++ Sprint2/upload_to_github.py
@@ -0,0 +1,17 @@
+# This is a script to upload files to GitHub
+
+import os
+import sys
+
+def upload_file(file_path):
+    if os.path.exists(file_path):
+        print(f'Uploading {file_path}...')
+        # Code to upload file to GitHub would go here
+    else:
+        print('File does not exist.')
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        upload_file(sys.argv[1])
+    else:
+        print('Please provide a file path to upload.'),
